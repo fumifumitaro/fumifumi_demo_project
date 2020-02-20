17f2be37c3e00d44d78240825817e3436fb5d703
小笠原 史人 <f.ogasawara012@gmail.com>
2020-02-20 14:02:31 +0900

    add: create top page

--- src/resources/js/Pages/Home/Index.vue
+++ src/resources/js/Pages/Home/Index.vue
@@ -1,3 +1,21 @@
 <template>
-        <h1>Welcome</h1>
+    <div>
+        <header>
+            <div class="items-right flex">
+                <div class="header-links items-under" style="padding: 1em">
+                    <a href="">Home</a>
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </div>
+            </div>
+        </header>
+
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
