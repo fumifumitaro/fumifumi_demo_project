4f155c02dcbac12fe422dbe7ca8be6206aa17eef
小笠原 史人 <f.ogasawara012@gmail.com>
2020-02-25 15:50:16 +0900

    add: organize top page

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -3,6 +3,10 @@ import { InertiaApp } from '@inertiajs/inertia-vue'
 
 const app = document.getElementById('app')
 const page = JSON.parse(app.dataset.page)
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI)
 
 new Vue({
     render: h =>
