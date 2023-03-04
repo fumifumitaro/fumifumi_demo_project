431d06a1fbf3a8dc84a28a9a5e937771edaabaf0
山田 <60929549+Shinniti1018@users.noreply.github.com>
2023-03-04 18:36:34 +0900

    dd2 commit

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
     }
 
     private function fetchArticles()
-    {   dd(User::all());
+    {   /*dd(User::all());*/
         return Article::with('user')
             ->orderBy('created_at')
             ->get()
