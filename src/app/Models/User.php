c261d776aa6f8fdce5ce1cea728c13ef42364504
fumifumitaro <51777014+fumifumitaro@users.noreply.github.com>
2023-03-04 19:22:50 +0900

    yrtfcjgkf

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function debug(){
+        dd('デバッグしていますよ。');
+    }
 }
