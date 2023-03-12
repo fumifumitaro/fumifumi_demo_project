9dbe686cbc217faca85caa433975533ff924943c
山田 <60929549+Shinniti1018@users.noreply.github.com>
2023-03-12 14:50:08 +0900

    -commit

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address');//住所追加
+            
             $table->rememberToken();
             
             $table->softDeletes();
