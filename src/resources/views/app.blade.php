214a07f2cf1466ebd9bd2ce2d6799416daa096c8
山田 <60929549+Shinniti1018@users.noreply.github.com>
2023-03-26 20:07:42 +0900

    commit adress --

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -9,26 +9,5 @@
 </head>
 <body>
 @inertia
-<div class="form-group row"><!-- 追加-->
-  <label for="phone" class="col-md-4 col-form-label text-md-right">電話番号</label>
-  <div class="col-md-6">
-    <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
-    @if ($errors->has('phone'))
-      <span class="invalid-feedback">
-        <strong>{{ $errors->first('phone') }}</strong>
-      </span>
-    @endif
-  </div>
-</div>
-<div class="form-group row">
-  <label for="address" class="col-md-4 col-form-label text-md-right">住所</label>
-  <div class="col-md-6">
-    <input id="address" type="text" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="address" value="{{ old('address') }}" required autofocus>
-    @if ($errors->has('address'))
-      <span class="invalid-feedback">
-        <strong>{{ $errors->first('address') }}</strong>
-      </span>
-    @endif
-  </div>
-</div><!--   追加-->
+
 </html>
