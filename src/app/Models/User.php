bf67b51f18ca1d4ebf9a0c37e64fbc720af94856
山田 <60929549+Shinniti1018@users.noreply.github.com>
2023-04-04 06:50:55 +0900

    address commit

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -20,8 +20,9 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'phone', 'address',//phone,address追加
+        'name', 'email', 'password', 'address',//address追加
     ];
+
 
     /**
      * The attributes that should be hidden for arrays.
