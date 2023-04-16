43a74e860fe322c9089c580e6f6bdfe6f0a2bbae
山田 <60929549+Shinniti1018@users.noreply.github.com>
2023-04-16 14:45:26 +0900

    内部を解析 controller

--- src/app/Http/Controllers/WorksController.php
+++ src/app/Http/Controllers/WorksController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers\Works;
 
 use App\Http\Controllers\Controller;
 
-use App\Models\User;
-use Auth;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
+use App\Models\User; //ユーザーモデルクラス
+use Auth; //ユーザー認証に必要
+use Illuminate\Http\Request; //データの格納処理
+use Inertia\Inertia; //Inertia.jsにてユーザー登録画面やログイン画面の生成
 
 class UserController extends Controller
 {
     public function edit()//ページを作り出す
     {
-    $user = Auth::user();
+    $user = Auth::user(); //ユーザー認証　ログインしていればログインしているユーザーのモデルクラスを返す。ログインしていなければnull。
 
-    return Inertia::render('User/Edit',[
-        'user' =>$user
+    return Inertia::render('User/Edit',[ //Inertia.jsにてユーザー登録画面やログイン画面の生成
+        'user' =>$user //=>は連想配列の処理?
     ]);
 }
     public function update(Request $request)//worksテーブルにデータを格納するための処理
@@ -24,7 +24,7 @@ class UserController extends Controller
         logger($request->all());// $requestにデータ格納
 
             User::update([
-                'works' => $request->works
+                'works' => $request->works //=>は連想配列の処理
             ]);
         return[];    
     }    
