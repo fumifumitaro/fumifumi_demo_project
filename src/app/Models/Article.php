f7867ada0ad757066535c8d2f209eb92912fae9c
yuma533 <o.g.w.y.u.m2262@gmail.com>
2023-05-29 13:58:46 +0900

    データベースにbookmarkインサートする機能追加しました。

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -22,4 +22,8 @@ class Article extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function user_bookmarks(){
+        return $this->hasOne(UserBookmark::class);
+    }
 }
